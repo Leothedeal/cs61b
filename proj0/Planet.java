814aa40f1acb70f871a57ca95bf27f51e29a694b
Zhong Liu <834502360@qq.com>
2020-11-20 20:32:48 +0800

    finish project0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,10 +1,10 @@
 public class Planet {
-    double xxPos;
-    double yyPos;
-    double xxVel;
-    double yyVel;
-    double mass;
-    String imgFileName;
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
         this.xxPos = xP;
@@ -16,6 +16,7 @@ public class Planet {
     }
 
     public Planet(Planet p) {
+        this.xxPos = p.xxPos;
         this.yyPos = p.yyPos;
         this.xxVel = p.xxVel;
         this.yyVel = p.yyVel;
